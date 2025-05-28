f9763e1f8401263ebe82e2a76d8a2eb93bfbbd42
smallwhitelz <zhanglboy1228@163.com>
2025-05-28 14:20:15 +0800

    add context

--- 12_context/context_test.go
+++ 12_context/context_test.go
@@ -12,11 +12,13 @@ func TestContextValue(t *testing.T) {
 	t.Log(val, ok)
 }
 
+// 主动取消
 func TestContextCancel(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	go func() {
 		time.Sleep(time.Second)
 		t.Log("准备调用cancel了")
+		// 关掉ctx.Done()的channel，从而进入到23行
 		cancel()
 	}()
 	<-ctx.Done()
@@ -24,6 +26,7 @@ func TestContextCancel(t *testing.T) {
 	t.Log(ctx.Err())
 }
 
+// 超时
 func TestContextTimeout(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
@@ -33,6 +36,7 @@ func TestContextTimeout(t *testing.T) {
 	t.Log(ctx.Err())
 }
 
+// 控制是从上到下，父结束，子也结束
 func TestContextParentCancel(t *testing.T) {
 	parent, cancel := context.WithCancel(context.Background())
 	time.AfterFunc(time.Second, func() {
@@ -44,6 +48,7 @@ func TestContextParentCancel(t *testing.T) {
 	sonCancel()
 }
 
+// panic
 func TestContextParentCancel1(t *testing.T) {
 	parent, cancel := context.WithCancel(context.Background())
 	_, sonCancel := context.WithCancel(parent)
@@ -55,10 +60,12 @@ func TestContextParentCancel1(t *testing.T) {
 	cancel()
 }
 
+// 取值是从下至上，儿子现在自己哪里找，找不到再去父亲哪里找
 func TestContextParentValue(t *testing.T) {
 	parent := context.WithValue(context.Background(), "key1", "value1")
 	son := context.WithValue(parent, "key1", "son-value1")
 	t.Log(son.Value("key1"))
+	// key2在son中，parent取不到
 	_ = context.WithValue(parent, "key2", "son-value2")
 	t.Log(parent.Value("key2"))
 }
